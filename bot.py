41ae9d04f8cfe88836fea4f3fcaaf7e0d1a2a4c5
n3at-fr33k <40575384+n3at-fr33k@users.noreply.github.com>
2018-08-31 14:48:28 -0400

    HereYouGoSmurf

---
+++ bot.py
@@ -0,0 +1,31 @@
+import discord
+import asyncio
+from discord.ext.commands import Bot
+from discord.ext import commands
+import platform
+
+#This line defines the client variable which will be used a lot in coding commands.
+client = Bot(description = "ResoBot made by On!on#5333", command_prefix = "reso ")
+
+@client.event
+async def on_ready():
+    #This function sets the Playing status for the bot
+    await client.change_presence(activity = discord.Game("in development v1.0.0 alpha"))
+    #The following lines print some information about the bot to the console
+    print("=" * 40 + "\n")
+    print("Logged in as: {}".format(client.user.name))
+    print("Bot's clientID: {}\n".format(client.user.id))
+    print("Connected to {} guilds".format(len(client.guilds)))
+    print("Connected to {} users\n".format(sum(1 for user in client.get_all_members())))
+    print("Running discord.py version {}".format(discord.__version__))
+    print("Running Python version {}\n".format(platform.python_version()))
+    print("Invite your bot: https://discordapp.com/oauth2/authorize?client_id={}&scope=bot&permissions=8\n".format(client.user.id))
+    print("="  * 40)
+
+#Basic ping command - if you type ping with your prefix, it'll respond
+@client.command()
+async def ping(ctx):
+    await ctx.send(":ping_pong: **Pong!**")
+
+#Every bot has a unique token - insert your bot's token within the quotation marks
+client.run("NoStealingMyTokenSmurf;/")
